1c11183cda9f139e2d687da04bc0fe4c951bd094
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-15 23:12:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+document.querySelector("#evaluatedText").addEventListener("input",()=>{
+   let len = document.querySelector("#evaluatedText").value.length;
+
+   document.querySelector("#letterCount").innerHTML = len;
+});
